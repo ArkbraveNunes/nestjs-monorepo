2cf5071004d332814d13277554fdb6f135219500
Jean Nunes <jeanclaudionunessantosnunes@gmail.com>
2024-02-10 20:25:43 -0300

    feat(DOMAIN): ✨ application-example | create service user.create, dto and tests

---
+++ apps/application-example/src/module/application-example/domain/service/user/create/user.create.service.spec.ts
@@ -0,0 +1,74 @@
+import { InternalServerErrorException } from '@nestjs/common';
+import { MockProxy, mock } from 'jest-mock-extended';
+
+import { mockedUserEntity, mockedAddressEntity } from '@test/mock';
+import {
+  UserCreateService,
+  UserCreateServiceInputDto,
+} from '@applicationExample/domain/service';
+import { UserRepositoryContract } from '@applicationExample/domain/contract';
+import { MESSAGES_ERRORS, MESSAGES_SUCCESS } from '@common/enum';
+import { CryptographDataService } from '@libs/cryptograph-data';
+
+describe('UserCreateService', () => {
+  let service: UserCreateService;
+  let customerRepository: MockProxy<UserRepositoryContract>;
+  let cryptographDataService: MockProxy<CryptographDataService>;
+
+  const userCreateServiceInputDto: UserCreateServiceInputDto = {
+    ...mockedUserEntity.profile,
+    address: mockedAddressEntity(),
+  };
+
+  beforeEach(() => {
+    customerRepository = mock();
+    customerRepository.create.mockResolvedValue(void 0);
+
+    cryptographDataService = mock();
+    cryptographDataService.encryptData.mockResolvedValue(
+      mockedUserEntity.profile.password,
+    );
+
+    service = new UserCreateService(customerRepository, cryptographDataService);
+  });
+
+  it('status 200 - success', async () => {
+    const actualResult = await service.execute(userCreateServiceInputDto);
+
+    expect(cryptographDataService.encryptData).toHaveBeenCalledTimes(1);
+    expect(cryptographDataService.encryptData).toHaveBeenCalledWith(
+      userCreateServiceInputDto.password,
+    );
+    expect(customerRepository.create).toHaveBeenCalledTimes(1);
+    expect(actualResult.id).toBeDefined();
+    expect(
+      actualResult.message.includes(MESSAGES_SUCCESS.USER_CREATED_WITH_SUCCESS),
+    ).toBe(true);
+  });
+
+  it('error status 500 - cryptographDataService - internal server error', async () => {
+    cryptographDataService.encryptData.mockRejectedValue(
+      new InternalServerErrorException(MESSAGES_ERRORS.INTERNAL_SERVER_ERROR),
+    );
+
+    await service
+      .execute(userCreateServiceInputDto)
+      .catch((actualError) => {
+        expect(actualError).toBeInstanceOf(InternalServerErrorException);
+      })
+      .then((result) => expect(result).toBe(undefined));
+  });
+
+  it('error status 500 - database - internal server error', async () => {
+    customerRepository.create.mockRejectedValue(
+      new InternalServerErrorException(MESSAGES_ERRORS.INTERNAL_SERVER_ERROR),
+    );
+
+    await service
+      .execute(userCreateServiceInputDto)
+      .catch((actualError) => {
+        expect(actualError).toBeInstanceOf(InternalServerErrorException);
+      })
+      .then((result) => expect(result).toBe(undefined));
+  });
+});
---
+++ apps/application-example/src/module/application-example/domain/service/user/index.ts
@@ -0,0 +1,2 @@
+export * from './create/user.create.service';
+export * from './create/user.create.service.dto';
