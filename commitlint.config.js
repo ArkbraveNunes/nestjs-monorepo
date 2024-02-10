1d0eb61a8e17135d74f38d6caf14e0a992d61de1
Jean Nunes <jeanclaudionunessantosnunes@gmail.com>
2024-02-10 15:23:46 -0300

    feat(CONFIG): ✨ config | update vscode settings, add new packages and fix jest config

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,6 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'scope-case': [2, 'always', ['upper-case']],
-    'scope-min-length': [2, 'always', 5],
+    'scope-min-length': [2, 'always', 1],
   },
 };
